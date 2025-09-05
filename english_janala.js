777291116ba37fb3757d7c305cc22cb24ed43870
sumaiya <sumaiyamoina@gmail.com>
2025-09-05 23:28:28 +0600

    adding lesson buttons

--- english_janala.js
+++ english_janala.js
@@ -1,1 +1,31 @@
-console.log("connected")
+const loadData=()=>
+{
+    fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then(res=>res.json())
+    .then(json=>showBtnData(json.data))
+}
+
+loadData()
+
+
+const showBtnData=(lessons)=>
+{
+   
+    const btnContainer=document.getElementById('btnDiv-container')
+
+    btnContainer.innerHTML=" ";
+
+    lessons.forEach(lesson => {
+
+        console.log(lesson)
+
+        const lessonBtn=document.createElement('button');
+        lessonBtn.innerHTML=`
+         <button href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book"></i>Lesson - ${lesson.level_no}</button>
+
+        `
+        btnContainer.appendChild(lessonBtn)
+        
+    });
+
+}
